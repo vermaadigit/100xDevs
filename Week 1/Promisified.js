31af6eedf4508e2f4c6fa86042efe9aba6294713
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-06 13:57:46 +0530

    Week 1 Promisified JS

--- Week 1/Promisified.js
+++ Week 1/Promisified.js
@@ -1,9 +1,25 @@
-function random()
-{
+// function random()
+// {
 
+// }
+
+// let p = new Promise(random);
+
+
+// console.log(p);
+
+function random(resolve)
+{
+    setTimeout(() => {
+        resolve();
+    }, 3000)
 }
 
 let p = new Promise(random);
 
+function main()
+{
+    console.log("Main Function is Called !!")
+}
 
-console.log(p);
+p.then(main)

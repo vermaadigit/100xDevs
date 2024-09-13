10bd4aab6d30ebaeca416b5d6960f711c06c6054
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-14 00:39:22 +0530

    Index file updated

--- Week 3/index.js
+++ Week 3/index.js
@@ -1,9 +1,26 @@
 const express = require("express")
 
+function calculateSum(n)
+{
+    let ans = 0
+    for (let i = 0; i < n; i++)
+    {
+        ans = ans + i
+    }
+    return ans
+}
+
+
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    const n = req.query.n
+    const ans = calculateSum(n)
+    res.send(ans.toString())
 })
 
-app.listen(3000)
+app.get('/test', function(req, res) {
+    res.send('Test routes is Here')
+})
+
+app.listen(3000) 

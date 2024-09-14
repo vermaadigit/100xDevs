2154fb2687c9060b05d4efa0f01001855d7da30a
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-14 23:46:08 +0530

    Index File updated

--- Week 4/index.js
+++ Week 4/index.js
@@ -11,7 +11,34 @@ const user = [{
 
 app.get('/', function(req, res) {
     const johnKidneys = user[0].kidneys;
-    
+    const numberofKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+
+    for (let i = 0; i < johnKidneys.length; i++)
+    {
+        if (johnKidneys[i].healthy)
+        {
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1
+        }
+    }
+
+    const numberOfUnhealthyKidneys = numberofKidneys - numberOfHealthyKidneys
+    res.json ({
+        numberofKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+            
+})
+
+app.post('/', function (req, res) {
+    const isHealthy = req.body.isHealthy
+    user[0].kidneys.push ({
+        healthy : isHealthy
+    })
+    res.json ({
+        msg : 'Done!'
+    })
 })
 
 app.listen(3000)

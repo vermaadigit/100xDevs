9f30b2a6d220723838cbbe3f541d519e938e55db
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-16 16:43:50 +0530

    MiddleWres

--- Week 5/app.js
+++ Week 5/app.js
@@ -30,4 +30,20 @@ app.get('/ride1', function(req, res) {
     }
 })
 
+app.get('/ride2', function(req, res) {
+
+    if(enoughAge(req.query.age))
+    {
+        res.json({
+            msg : "You have successfully riden the ride 1"
+        })
+    }
+    else
+    {
+        res.status(411).json({
+            msg : "Sorry now you are not elligible to ride"
+        })
+    }
+})
+
 app.listen(3000)

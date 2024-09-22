8ae16d14f72b93c68120a9864f1eb34c30e6b200
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-22 18:05:17 +0530

    Simple Auth Function

---
+++ Week 8/Authentication/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const users = []
+
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post('/signup', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password; 
+
+    users.push({
+        username: username,
+        password: password
+    });
+
+    res.json({
+        msg : 'You are Signed Up'
+    })
+
+    // if (users.find(user => user.username === username)) {
+    //     res.json({
+    //         msg: 'User already exists'
+    //     })
+    // }
+
+    console.log(users)
+})
+
+app.post('/signin', function(req, res) {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser)
+    {
+        const token = generateToken()
+        foundUser.token = token
+
+        res.json({
+            token : token
+        })
+        
+    }
+    else
+    {
+        res.status(403).send({
+            msg : 'Invalid Username or Password'
+        })
+    }
+
+    // const user = users.find(function(user) {
+    //     return user.username === username && user.password === password;
+    // })  
+
+    console.log(users)
+})
+
+app.get('/me', function(req, res) {
+
+    const token = req.headers.token
+    const foundUser = null
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].token === token) {
+            foundUser = users[i]
+        }
+    }
+
+})
+
+app.listen(3000)

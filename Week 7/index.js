0ff715ef882d2cf9ad66e6291beeefbfce896a31
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-22 11:53:29 +0530

    Fetch API

---
+++ Week 7/index.js
@@ -0,0 +1,9 @@
+
+async function main()
+{
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json()
+    console.log(data.id)
+}
+
+main()

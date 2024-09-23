83c4939399831a360cee7677dca169d33dea2f90
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-23 15:40:09 +0530

    JWT Install

--- Week 8/Authentication/index.js
+++ Week 8/Authentication/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'randomaditya'
 
 const app = express();
 app.use(express.json());
 
 const users = []
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 
 app.post('/signup', function(req, res) {
     const username = req.body.username;
@@ -53,8 +55,12 @@ app.post('/signin', function(req, res) {
 
     if (foundUser)
     {
-        const token = generateToken()
-        foundUser.token = token
+        // const token = generateToken()
+        const token = jwt.sign({
+            username : username
+        }, JWT_SECRET);
+
+        // foundUser.token = token
 
         res.json({
             token : token
@@ -78,14 +84,18 @@ app.post('/signin', function(req, res) {
 app.get('/me', function(req, res) {
 
     const token = req.headers.token
+    const decodedInformation = jwt.verify(token, JWT_SECRET)
+    const username = decodedInformation.username
+
+
     let foundUser = null
 
     for (let i = 0; i < users.length; i++) {
-        if (users[i].token === token) {
+        if (users[i].username === username) {
             foundUser = users[i]
         }
     }
-
+    
     if (foundUser)
     {
         res.json ({
@@ -99,6 +109,8 @@ app.get('/me', function(req, res) {
             message : 'Invalid Token'
         })
     }
+
+
 })
 
 app.listen(3000)

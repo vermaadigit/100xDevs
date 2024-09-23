84d8305c6807d824b60d855245e82f817188fc4e
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-23 18:21:01 +0530

    HTTP Server JWT

---
+++ Week 8/Authentication-part2/app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+
+
+const users = []
+
+app.post('/signup', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        msg: 'You are Signed Up'
+    })
+
+    console.log(users)
+
+})
+
+app.post('login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null
+
+    for (let i = 0; i < users.length; i++)
+    {
+        if (users[i].username === username && users[i].password === password)
+        {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser)
+    {
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+
+        res.json ({
+            token : token
+        })
+    } else {
+        res.status(403).send ({
+            msg : 'Inavlid Username or Password'
+        })
+    }
+
+    console.log(users)
+})
+
+app.get('/user', function(req, res) {
+    const token = req.headers.token;
+    const decodedInformation = jwt.verify(token, JWT_SECRET);
+
+    const username = decodedInformation.username;
+
+    let founbdUser = null
+
+    for (let i = 0; i < users.length; i++)
+    {
+        if (users[i].username === username)
+        {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser)
+    {
+        res.json({
+            username: foundUser.username,
+            password : foundUser.password
+        })
+    } else {
+        res.status(403).send ({
+            msg : 'Invalid Token'
+        })
+    }
+})
+
+app.listen(3000)

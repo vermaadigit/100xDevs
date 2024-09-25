c7fe61428592b7c30fdb4ebfdde030a680995891
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-09-25 17:23:58 +0530

    Auth Function Added

--- Week 8/Authentication-part2/app.js
+++ Week 8/Authentication-part2/app.js
@@ -15,6 +15,10 @@ function logger(req, res, next) {
     next()
 }
 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
 app.post('/signup', logger, function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
@@ -49,7 +53,7 @@ app.post('/login', logger, function(req, res) {
     if (foundUser)
     {
         const token = jwt.sign({
-            username: username
+            username: foundUser.username
         }, JWT_SECRET);
 
         res.header('token', token)
@@ -68,19 +72,44 @@ app.post('/login', logger, function(req, res) {
 })
 
 function auth(req, res, next) {
-    const token = req.headers.token;
-    const decodedInformation = jwt.verify(token, JWT_SECRET);
+    // const token = req.headers.token;
+    // const decodedInformation = jwt.verify(token, JWT_SECRET);
 
-    if (decodedInformation.username)
-    {
-        req.username = decodedInformation.username
-        next()
+    // if (decodedInformation.username)
+    // {
+    //     req.username = decodedInformation.username
+    //     next()
+    // }
+    // else
+    // {
+    //     res.status(403).send ({
+    //         msg : 'Invalid Token'
+    //     })  
+    // }
+
+    const token = req.headers.token;
+
+    if (!token) {
+        return res.status(400).send({
+            msg: 'Token must be provided'
+        });
     }
-    else
-    {
-        res.status(403).send ({
-            msg : 'Invalid Token'
-        })  
+
+    try {
+        const decodedInformation = jwt.verify(token, JWT_SECRET);
+
+        if (decodedInformation.username) {
+            req.username = decodedInformation.username;
+            next();
+        } else {
+            res.status(403).send({
+                msg: 'Invalid Token'
+            });
+        }
+    } catch (err) {
+        res.status(403).send({
+            msg: 'Invalid Token'
+        });
     }
 }
 
@@ -90,11 +119,13 @@ app.get('/user',logger, auth, function(req, res) {
 
     // const username = decodedInformation.username;
 
+    const currentUser = req.username
+
     let foundUser = null
 
     for (let i = 0; i < users.length; i++)
     {
-        if (users[i].username === req.username)
+        if (users[i].username === currentUser)
         {
             foundUser = users[i]
         }

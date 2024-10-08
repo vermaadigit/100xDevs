e56fa5f48f6d4eb9673d2fb14050b1edac03f7c4
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-08 17:59:09 +0530

    Database config. File Added

--- Week 10/MongoDB/db.js
+++ Week 10/MongoDB/db.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema
 const ObjectId = mongoose.ObjectId
 
 const User = new Schema ({
-    email : String,
-    password : String,
-    name : String
+    name : String,
+    email : {type : String, unique : true},
+    password : String
 })
 
 const Todo = new Schema ({

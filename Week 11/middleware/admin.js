14ccde5081ad8354ed255121f419e8f930de005f
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-13 19:52:33 +0530

    admin middleware added

--- Week 11/middleware/admin.js
+++ Week 11/middleware/admin.js
@@ -1,7 +1,25 @@
+const { Admin } = require('../db/index')
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username   //test@gmail.com
+    const password = req.headers.password   //test@123
+
+    Admin.findOne({
+        username,
+        password
+    })
+    .then(function(value) {
+        if(value) {
+            next()
+        } else {
+            res.status(403).json({
+                message : 'Incorrect Credential for Admin'
+            })
+        }
+    })
+
 }
 
 module.exports = adminMiddleware;

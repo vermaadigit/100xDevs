01c00978a0a8e3a6e05882651991f8a2033562c6
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-21 23:49:23 +0530

    app.jsx updated

--- Week 12/Week 12 React Project/src/App.jsx
+++ Week 12/Week 12 React Project/src/App.jsx
@@ -1,41 +1,62 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// useEffect, dependency array, cleanups
 function App() {
+
+
   return <div>
-    Hii There !!
     <Counter></Counter>
   </div>
 }
 
 
+// mounting, re-rendering, unmounting
+
 function Counter() {
+  
 
-  const [count, setCount] = useState(0);
+  useEffect(function() {
+    console.log("mount")
 
-  function increaseCount() {
-    setCount(count + 1)
-  }
-
-  function decreaseCount() {
-    setCount(count - 1)
-  }
-
-  function resetCount() {
-    setCount(0)
-  }
+    return function() {
+      console.log("unmount")
+    }
+  }, [])
 
   return <div>
-    <h1 id = "text" >{count}</h1>
-    <button onClick = {increaseCount} >Increase Count</button>
-    <button onClick = {decreaseCount} >Decrease Count</button>
-    <br></br>
-    <br></br>
-    <button onClick = {resetCount} >Reset Count</button>
+    Counter!
   </div>
 }
+
+
+// function Counter() {
+
+//   const [count, setCount] = useState(0);
+
+//   function increaseCount() {
+//     setCount(count + 1)
+//   }
+
+//   function decreaseCount() {
+//     setCount(count - 1)
+//   }
+
+//   function resetCount() {
+//     setCount(0)
+//   }
+
+//   return <div>
+//     <h1 id = "text" >{count}</h1>
+//     <button onClick = {increaseCount} >Increase Count</button>
+//     <button onClick = {decreaseCount} >Decrease Count</button>
+//     <br></br>
+//     <br></br>
+//     <button onClick = {resetCount} >Reset Count</button>
+//   </div>
+// }
 
 
 
